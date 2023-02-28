c760f8c4ef12c89dbcdb82f3ce57d938afa1442d
Victoria Kovalenko <v.kovalenkoooooo@gmail.com>
2023-02-28 22:01:52 +0200

    added database phones

--- db/seeders/20230228175713-demo-phone.js
+++ db/seeders/20230228175713-demo-phone.js
@@ -1,25 +1,19 @@
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
+const data = require('../../mockup_data/phones.json');
+
+const preaperedData = data.map(d => {
+  return {
+    ...d,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  }
+})
+
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Phones', [{
-      id: '1',
-      category: 'phones',
-      phoneId: 'apple-iphone-7-32gb-black',
-      itemId: 'apple-iphone-7-32gb-black',
-      name: 'Apple iPhone 7 32GB Black',
-      fullPrice: 400,
-      price: 375,
-      screen: "4.7' IPS",
-      capacity: '32GB',
-      color: 'black',
-      ram: '2GB',
-      year: 2016,
-      image: 'img/phones/apple-iphone-7/black/00.jpg',
-      createdAt: new Date(),
-      updatedAt: new Date()
-      }], {});
+    await queryInterface.bulkInsert('Phones', preaperedData , {});
   },
 
   async down (queryInterface, Sequelize) {
